f4fca3b6101bc4112a56302c047e407e71a73d2f
David Barragán Merino <david.barragan@kaleidos.net>
2014-12-30 18:25:12 +0100

    Create Member model

--- backend/kquotes/organizations/admin.py
+++ backend/kquotes/organizations/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Organization
+from .models import Member
+
+
+class MemberInline(admin.TabularInline):
+    model = Member
+    fields = ("organization", "user", "is_owner", "is_admin")
+    extra = 0
 
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ("slug",)
+    inlines = [MemberInline]
--- backend/kquotes/organizations/models.py
+++ backend/kquotes/organizations/models.py
@@ -5,11 +5,15 @@ from kquotes.base.utils.slug import slugify_uniquely
 
 
 class Organization(models.Model):
-    name = models.CharField(null=False, blank=True, max_length=256, verbose_name=_("name"))
-    slug = models.SlugField(null=False, blank=True, max_length=256, unique=True, verbose_name=_("slug"))
+    name = models.CharField(null=False, blank=True, max_length=256,
+                            verbose_name=_("name"))
+    slug = models.SlugField(null=False, blank=True, max_length=256, unique=True,
+                            verbose_name=_("slug"))
 
-    created_date = models.DateTimeField(null=False, blank=False, auto_now_add=True, verbose_name=_("created date"))
-    modified_date = models.DateTimeField(null=False, blank=False, auto_now=True, verbose_name=_("modified date"))
+    created_date = models.DateTimeField(null=False, blank=False, auto_now_add=True,
+                                        verbose_name=_("created date"))
+    modified_date = models.DateTimeField(null=False, blank=False, auto_now=True,
+                                         verbose_name=_("modified date"))
 
     class Meta:
         verbose_name = "organization"
@@ -21,5 +25,31 @@ class Organization(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify_uniquely(self.name, self, max_length=256)
+            self.slug = slugify_uniquely(self.name, self, max_length=self._meta.get_field("slug").max_length)
+        super().save(*args, **kwargs)
+
+
+class Member(models.Model):
+    user = models.ForeignKey("users.User", null=False, blank=False,
+                             related_name="memberships", verbose_name=_("user"))
+    organization = models.ForeignKey("organizations.Organization", null=False, blank=False,
+                                     related_name="members", verbose_name=_("organization"))
+
+    is_admin = models.BooleanField(null=False, blank=True, default=False,
+                                   verbose_name=_("is admin"))
+    is_owner = models.BooleanField(null=False, blank=True, default=False,
+                                   verbose_name=_("is owner"))
+
+    class Meta:
+        verbose_name = "member"
+        verbose_name_plural = "members"
+        ordering = ["organization", "user"]
+        unique_together = ("organization", "user")
+
+    def __str__(self):
+        return "{} - {}".format(self.organization, self.user)
+
+    def save(self, *args, **kwargs):
+        if self.is_owner:
+            self.is_admin = True
         super().save(*args, **kwargs)
