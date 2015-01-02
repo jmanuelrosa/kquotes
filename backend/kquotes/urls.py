84ac7aeb6bb55a6a938fd6e486fd402fb42a2e19
David Barragán Merino <david.barragan@kaleidos.net>
2015-01-02 16:29:09 +0100

    Create auth/token endpoint

--- backend/kquotes/urls.py
+++ backend/kquotes/urls.py
@@ -5,7 +5,8 @@ from .routers import router
 
 urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
-    url(r'^api/v1/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/v1/auth/token', 'rest_framework_jwt.views.obtain_jwt_token'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
