25b20ab38ac9113b8df5210192cff8adb58e763f
David Barragán Merino <david.barragan@kaleidos.net>
2017-01-06 13:24:07 +0100

    Create 'Add Quote' mutation

--- backend/kquotes/quotes/schema.py
+++ backend/kquotes/quotes/schema.py
@@ -1,8 +1,12 @@
 import graphene
-from graphene import AbstractType, Mutation, Node
+from graphene import relay, AbstractType, Mutation, Node
 
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
+
+from django.contrib.auth import get_user_model
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode as uid_decoder
 
 from .models import Quote
 from .models import Score
@@ -19,10 +23,46 @@ class ScoreNode(DjangoObjectType):
         model = Score
 
 
+class AddQuote(relay.ClientIDMutation):
+    ok = graphene.Boolean()
+    result = graphene.Field(QuoteNode)
+
+    class Input:
+        quote = graphene.String(required=True)
+        explanation = graphene.String()
+        author = graphene.ID()
+        external_author =  graphene.String()
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        quote = Quote(quote=input.get('quote'),
+                      explanation=input.get('explanation'),
+                      creator=context.user)
+
+        author_uuid = input.get('author', None)
+        external_author = input.get('external_author', None)
+        if author_uuid:
+            try:
+                id = force_text(uid_decoder(author_uuid))
+                quote.author = get_user_model.objects.get(id=id)
+            except (TypeError, ValueError, OverflowError):
+                raise Exception("'author'(ID) must be a valid user ID")
+            except get_user_model.DoesNotExist:
+                raise Exception("there is no user with this ID")
+        elif external_author:
+            quote.external_author = external_author
+        else:
+            raise Exception("you need tu set an 'author' (ID) or an 'external autor' (String)")
+
+        quote.save()
+
+        return AddQuote(ok=True, quote=quote)
+
+
 class QuotesQuery(AbstractType):
     quote = graphene.Field(QuoteNode)
     quotes = DjangoFilterConnectionField(QuoteNode)
 
 
 class QuotesMutation(AbstractType):
-    pass
+    add_quote = AddQuote.Field()
